38f8d3bea1c2f7e856b7522c7638aafe7b80c3ec
Dagmawi <dagijossy18@gmail.com>
2026-09-01 07:50:04 +0300

    chore: bump version to v1.0.5

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default {
     name: 'Coding Coach',
     slug: 'coding-coach',
     scheme: 'codingcoach',
-    version: '1.0.4',
+    version: '1.0.5',
     newArchEnabled: true,
     orientation: 'portrait',
     icon: './assets/icon.png',
